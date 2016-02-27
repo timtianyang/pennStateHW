ec02eb95f67582f981358290555f440b35bf99b4
Tianyang Chen <tianyangpenn@gmail.com>
2016-02-27 11:10:13 -0500

    updated parsing

--- ttfn.c
+++ ttfn.c
@@ -1,27 +1,62 @@
-#include <stdbool.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <getopt.h>
 
-int main(int argc, char *argv[])
-{
-    bool isCaseInsensitive = false;
-    int opt;
-    enum { CHARACTER_MODE, WORD_MODE, LINE_MODE } mode = CHARACTER_MODE;
+void print_usage() {
+    printf("Usage: --latency|--throughput [- p packagesize] [-r repeats] hostname\n");
+}
 
-    while ((opt = getopt(argc, argv, "ilw")) != -1) {
+int main(int argc, char *argv[]) {
+    int opt= 0;
+    int through_put = 0;
+    int latency = 0;
+    int repeat = 0;
+    int packet_size = 0;
+
+    //Specifying the expected options
+    //The two options l and b expect numbers as argument
+    static struct option long_options[] = {
+        {"latency",      no_argument,       0,  'l' },
+        {"throughput", no_argument,       0,  't' },
+        {"p", required_argument,       0,  'p' },
+        {"r",    required_argument, 0,  'r' },
+        {0,           0,                 0,  0   }
+    };
+
+    int long_index =0;
+    
+    
+    while ((opt = getopt_long(argc, argv,"ltp:r:", 
+                   long_options, &long_index )) != -1) {
         switch (opt) {
-        case 'i': isCaseInsensitive = true; break;
-        case 'l': mode = LINE_MODE; break;
-        case 'w': mode = WORD_MODE; break;
-        default:
-            fprintf(stderr, "Usage: %s [-ilw] [file...]\n", argv[0]);
-            exit(EXIT_FAILURE);
+             case 'l' : latency = 1;
+                 break;
+             case 't' : through_put = 1;
+                 break;
+             case 'r' : repeat = atoi(optarg); 
+                 break;
+             case 'p' : packet_size = atoi(optarg);
+                 break;
+             default: print_usage();
+                 exit(EXIT_FAILURE);
         }
     }
-
-    // Now optind (declared extern int by <unistd.h>) is the index of the first non-option argument.
-    // If it is >= argc, there were no non-option arguments.
-
-    // ...
+    if( latency )
+    {
+        printf("measuring latency\n");
+    }
+    else if ( through_put )
+    {
+        printf("measuring throughput\n");
+    }
+    if( latency && through_put )   
+    {
+        print_usage();
+        exit(EXIT_FAILURE);
+    } 
+    
+    printf("packet size is %d\n",packet_size);
+    printf("repeating %d times\n",repeat);
+    return 0;
 }
