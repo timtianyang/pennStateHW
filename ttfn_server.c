6d1d01822f06441c9d1cbb7d3952d3daf4c85202
timtianyang <soapcn@gmail.com>
2016-04-22 16:37:32 -0400

    added parsing on server

--- ttfn_server.c
+++ ttfn_server.c
@@ -17,6 +17,64 @@ int main(int argc, char *argv[])
     const int PORT = 13547;
     char buff[BUFSIZE];
     ssize_t rval = 0;
+    int no_delay = 0;
+    int* delays;
+    int size = 0;
+    size_t len = 0;
+    int i;
+
+    FILE * fp;
+    char * line = NULL;
+
+    if ( argc != 2 )
+    {
+	printf("You didn't specify a file name so the delay will be zero\n");
+	no_delay = 1;
+    }
+    else
+    {
+	printf("reading from file %s\n", argv[1]);
+	fp = fopen(argv[1], "r");
+	if (fp == NULL)
+	{
+	    no_delay = 1;
+	    printf("the file is not found, use zero delay\n");
+	}
+	else
+	{
+	    while (getline(&line, &len, fp) != -1) {
+		//printf("%s\n", line);
+		size++;
+	    }
+	
+	    printf("there are %d lines in the file\n",size);
+	    //fclose(fp);
+	    if ( size == 0 )
+	    {
+		printf("use no delay\n");
+		no_delay = 1;
+	    }
+	    else
+	    {
+		delays = malloc(sizeof(int) * size);
+		if ( delays == NULL )
+		{
+		    printf("not enough mem!\n");
+		    return -1;
+		}
+
+		rewind(fp);
+		i = 0;
+		while (getline(&line, &len, fp) != -1) {
+		    delays[i++] = atoi(line);
+		}
+		for (i=0;i<size;i++)
+		    printf("%d\n",delays[i]);
+		fclose(fp);
+	    }
+	}    
+
+    }
     
     
     /* Create socket */
