0b3b9a681e9fbc10d12b8cac104257a2ffe1e5f8
markhua <huazhi.zhang@me.com>
2016-04-22 16:36:42 -0400

    Update ttfn_client.c

--- ttfn_client.c
+++ ttfn_client.c
@@ -101,6 +101,24 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
+void recv_ack(int sock){
+    char * receive_buf;
+    char * ack = "r";
+    receive_buf = (char*) malloc(packet_size);
+    memset(receive_buf, 0, sizeof(receive_buf));
+    int rec;
+    if((rec = recv(sock, receive_buf, packet_size, 0)) < 0){
+        perror("receiving failed");
+    }
+    while(strcmp(receive_buf,ack) != 0){
+        //perror("received no ACK");
+        if((rec = recv(sock, receive_buf, packet_size, 0)) < 0){
+            perror("receiving failed");
+        }
+    }
+    printf("Received: %s\n", receive_buf);
+}
+
 void ttfn_latency(){
     int sock;
     struct sockaddr_in server;
@@ -109,10 +127,7 @@ void ttfn_latency(){
     double cpu_time_used;
     
     char * buf;
-    char * receive_buf;
-    char * ack = "r";
     
-    receive_buf = (char*) malloc(packet_size);
     buf = (char*) malloc(packet_size);
     if (buf == NULL) exit (1);
     
@@ -139,21 +154,16 @@ void ttfn_latency(){
     long double sum = 0;
     int count = 0;
     
-    int i, rec;
+    int i;
     for ( i = 0; i<repeat; i++){
         start = clock();
         if((n =send(sock, buf, packet_size, 0)) < 0){
             perror("send failed");
             exit(1);
         }
-        memset(receive_buf, 0, sizeof(receive_buf));
-        if((rec = read(sock, receive_buf, packet_size)) < 0){
-            perror("receiving failed");
-        }
-        if(strcmp(receive_buf,ack) != 0){
-            perror("received no ACK");
-        }
+        recv_ack(sock);
         end = clock();
+        printf("start = %lu, end = %lu\n", start, end);
         cpu_time_used = ((double) (end - start)/CLOCKS_PER_SEC);
         printf("%d bytes from %s(%s): time= %Lf ms\n", (int)n, hp->h_name, hostip, (long double)cpu_time_used*1000);
         fflush(stdout);
@@ -181,10 +191,6 @@ void ttfn_throughput(){
     double cpu_time_used;
     
     char * buf;
-    char * receive_buf;
-    char * ack = "r";
-    
-    receive_buf = (char*) malloc(packet_size);
     buf = (char*) malloc(packet_size);
     if (buf == NULL) exit (1);
     
@@ -209,21 +215,16 @@ void ttfn_throughput(){
     long double max = 0;
     long double min = 1000;
     long double sum = 0;
-    int i, rec;
+    int i;
     for (i = 0; i<repeat; i++){
         start = clock();
         if((n =send(sock, buf, packet_size, 0)) < 0){
             perror("send failed");
             exit(1);
         }
-        memset(receive_buf, 0, sizeof(receive_buf));
-        if((rec = read(sock, receive_buf, packet_size)) < 0){
-            perror("receiving failed");
-        }
-        if(strcmp(receive_buf,ack) != 0){
-            perror("received no ACK");
-        }
+        recv_ack(sock);
         end = clock();
+        printf("start = %lu, end = %lu\n", start, end);
         cpu_time_used = ((double) (end - start)/CLOCKS_PER_SEC);
         printf("Sent %d bytes  time %Lf s\n", (int)n, (long double)cpu_time_used);
         fflush(stdout);
